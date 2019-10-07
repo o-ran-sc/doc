17962de9840e0e3229d93fe3cc82afa8d20cd3e3
Aric Gardner <agardner@linuxfoundation.org>
2019-10-07 14:48:19 -0400

    Add intersphinx_mapping
    This was we can reference the ric project from the index of
    the doc project
    
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: I8ed91f83ac20a11b1e998feb759342f8956527ff

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
 from docs_conf.conf import *
+
+branch = 'latest'
+
+linkcheck_ignore = [
+      'http://localhost',
+]
+
+intersphinx_mapping = {}
+intersphinx_mapping['portal-ric-dashboard'] = ('https://o-ran-sc-doc-test.readthedocs.io/projects/o-ran-sc-portal-ric-dashboard-test/en/%s' % branch, None)
