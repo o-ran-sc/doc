91639e72b3d7e8c9132701660390fcb5a96b6262
Lott, Christopher (cl778h) <cl778h@att.com>
2019-11-01 08:53:00 -0400

    Repair site name to drop -docs suffix
    Docs are available at o-ran-sc.readthedocs.io
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: I4c279385200ef24cc3716ec881e4055a0445abbb

--- docs/conf.py
+++ docs/conf.py
@@ -7,4 +7,4 @@ linkcheck_ignore = [
 ]
 
 intersphinx_mapping = {}
-intersphinx_mapping['portal-ric-dashboard'] = ('https://o-ran-sc-doc.readthedocs.io/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
+intersphinx_mapping['portal-ric-dashboard'] = ('https://o-ran-sc.readthedocs.io/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
