f840aa972d10745ab0359c6bc1f3f554a4f1f071
wrider <lji@research.att.com>
2019-11-03 14:10:50 -0500

    Add doc index for it/dep
    Change-Id: I3f345878ad6f3cc830d91f68af3301537114d42f
    Signed-off-by: wrider <lji@research.att.com>

--- docs/conf.py
+++ docs/conf.py
@@ -8,3 +8,4 @@ linkcheck_ignore = [
 
 intersphinx_mapping = {}
 intersphinx_mapping['portal-ric-dashboard'] = ('https://o-ran-sc-doc.readthedocs.io/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
+intersphinx_mapping['it-dep'] = ('https://o-ran-sc-doc.readthedocs.io/projects/o-ran-sc-it-dep/en/%s' % branch, None a)
