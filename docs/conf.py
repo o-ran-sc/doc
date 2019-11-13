a9f86e37384d16fa3b670839c9112d764d2be226
wrider <lji@research.att.com>
2019-11-12 23:35:47 -0500

    Add index mapping for it/* repos
    Change-Id: I37f9014cfd00a4a90ef65bac74d3b05eb0d0aefd
    Signed-off-by: wrider <lji@research.att.com>

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,8 @@ intersphinx_mapping = {}
 intersphinx_mapping['ric-app-mc'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-ric-app-mc/en/%s' % branch, None)
 
 intersphinx_mapping['portal-ric-dashboard'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
+
+intersphinx_mapping['it-dep'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-it-dep/en/%s' % branch, None)
+intersphinx_mapping['it-dev'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-it-dev/en/%s' % branch, None)
+intersphinx_mapping['it-test'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-it-test/en/%s' % branch, None)
+intersphinx_mapping['it-otf'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-it-otf/en/%s' % branch, None)
