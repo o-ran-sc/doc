b33cedc1a935d381e5a8c95c95715bf3494e644c
weichen <niweichen@chinamobile.com>
2019-11-12 20:51:53 -0500

    revert conf.py intersphinx URL, it was a mistake in last change
    Signed-off-by: weichen <niweichen@chinamobile.com>
    Change-Id: Ic46248d6b231db9d67217ad402bcf29fcea6f66f

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,7 @@ linkcheck_ignore = [
 ]
 
 intersphinx_mapping = {}
-intersphinx_mapping['ric-app-mc'] = ('https://o-ran-sc-doc.readthedocs.io/projects/o-ran-sc-ric-app-mc/en/%s' % branch, None)
 
-intersphinx_mapping['portal-ric-dashboard'] = ('https://o-ran-sc-doc.readthedocs.io/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
+intersphinx_mapping['ric-app-mc'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-ric-app-mc/en/%s' % branch, None)
+
+intersphinx_mapping['portal-ric-dashboard'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-portal-ric-dashboard/en/%s' % branch, None)
